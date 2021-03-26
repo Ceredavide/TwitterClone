c50865b82fe5424254b63f4d0bf5fa4327933256
Davide <ceredavide@live.it>
2021-03-26 14:29:51 +0100

    modificato stile bottoni

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12 pt-2">
-                <a href="/blog" class="btn btn-outline-primary btn-sm">Go back</a>
-                <h1 class="display-one">{{ ucfirst($post->title) }}</h1>
-                <p>{!! $post->body !!}</p> 
-                <hr>
-                <a href="/blog/{{ $post->id }}/edit" class="btn btn-outline-primary">Edit Post</a>
-                <br><br>
-                <form id="delete-frm" class="" action="" method="POST">
-                    @method('DELETE')
-                    @csrf
-                    <button class="btn btn-danger">Delete Post</button>
-                </form>
+<div class="container">
+    <div class="row">
+        <div class="col-12 pt-2">
+            <div class="row">
+                <div class="col-2">
+                    <a href="/blog" class="btn btn-primary btn">Go back</a>
+                </div>
+                <div class="col-6"></div>
+                <div class="col-2">
+                    <a href="/blog/{{ $post->id }}/edit" class="btn btn-warning text-white">Edit Post</a>
+                </div>
+                <div class="col-2">
+                    <form id="delete-frm" class="" action="" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn btn-danger">Delete Post</button>
+                    </form>
+                </div>
             </div>
+            <h1 class="display-one">{{ ucfirst($post->title) }}</h1>
+            <p>{!! $post->body !!}</p>
+            <hr>
+            <br><br>
         </div>
     </div>
+</div>
 @endsection
