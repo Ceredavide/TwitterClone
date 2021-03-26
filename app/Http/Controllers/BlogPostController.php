0a601c2115f01b8cd83a22c38e73031af6916572
Davide <ceredavide@live.it>
2021-03-26 09:25:11 +0100

    aggiunto azioni api blogpost

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -9,41 +9,64 @@ class BlogPostController extends Controller
 {
     public function index()
     {
-        $posts = BlogPost::all(); //fetch all blog posts from DB
-        return $posts; //returns the fetched posts
+        //fetch all blog posts from DB
+        $posts = BlogPost::all();
+
+        //ritorna view blog
+        return view('blog.index', [
+            'posts' => $posts,
+        ]);
     }
 
     public function create()
     {
-        //show form to create a blog post
+        return view('blog.create');
     }
 
 
     public function store(Request $request)
     {
-        //store a new post
+        $newPost = BlogPost::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            //TODO: Reperire user_id da auth
+            'user_id' => 1
+        ]);
+
+        return redirect('blog/' . $newPost->id);
     }
 
     public function show(BlogPost $blogPost)
     {
-        return $blogPost;
+        return view('blog.show', [
+            'post' => $blogPost,
+        ]); //returns the view with the post
     }
 
 
     public function edit(BlogPost $blogPost)
     {
-        //show form to edit the post
+        return view('blog.edit', [
+            'post' => $blogPost,
+        ]); //returns the edit view with the post
     }
 
 
     public function update(Request $request, BlogPost $blogPost)
     {
-        //save the edited post
+        $blogPost->update([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return redirect('blog/' . $blogPost->id);
     }
 
 
     public function destroy(BlogPost $blogPost)
     {
-        //delete a post
+        $blogPost->delete();
+
+        return redirect('/blog');
     }
 }
