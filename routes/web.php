c9115e4acb2b9cb13c00ac21e9dcc91b6ab8bc21
Davide <ceredavide@live.it>
2021-04-02 17:07:29 +0200

    aggiunte routes blogpost

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use \App\Http\Controllers\BlogPostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+
+    Route::get('/dashboard', [BlogPostController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard/{blogPost}', [BlogPostController::class, 'show']);
+
+    Route::get('/blog', [BlogPostController::class, 'add']);
+    Route::post('/blog', [BlogPostController::class, 'create']);
+
+    Route::get('/blog/{blogPost}', [BlogPostController::class, 'edit']);
+    Route::post('/blog/{blogPost}', [BlogPostController::class, 'update']);
+});
