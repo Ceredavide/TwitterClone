89e6eab3a5e0af5ec5f14f3d0c6e537644d1b67d
Davide <ceredavide@live.it>
2021-04-02 17:06:36 +0200

    aggiunto metodi controller

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -2,51 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use App\Models\BlogPost;
 
 class BlogPostController extends Controller
 {
-
     public function index()
     {
         $posts = BlogPost::all();
-        return view('blog.index', [
-            'posts' => $posts,
-        ]);
-    }
-
-    public function create()
-    {
-        //show form to create a blog post
-    }
-
-
-    public function store(Request $request)
-    {
-        //store a new post
+        return view('dashboard', ['posts' => $posts]);
     }
 
     public function show(BlogPost $blogPost)
     {
-        return $blogPost;
+        return view('blog.show', ['post' => $blogPost]);
     }
 
+    public function add()
+    {
+        return view("blog.add");
+    }
+
+    public function create(Request $request)
+    {
+        $blogPost = new BlogPost();
+        $blogPost->title = $request->title;
+        $blogPost->body = $request->body;
+        $blogPost->user_id = auth()->user()->id;
+        $blogPost->save();
+        return redirect('/dashboard');
+    }
 
     public function edit(BlogPost $blogPost)
     {
-        //show form to edit the post
+        if (auth()->user()->id == $blogPost->user_id) {
+            return view('blog.edit', ['post' => $blogPost]);
+        } else {
+            return redirect('/dashboard');
+        }
     }
-
 
     public function update(Request $request, BlogPost $blogPost)
     {
-        //save the edited post
-    }
-
-
-    public function destroy(BlogPost $blogPost)
-    {
-        //delete a post
+        if (auth()->user()->id == $blogPost->user_id) {
+            if (isset($_POST['delete'])) {
+                $blogPost->delete();
+                return redirect('/dashboard');
+            } else {
+                $blogPost->title = $request->title;
+                $blogPost->body = $request->body;
+                $blogPost->save();
+                return redirect('/dashboard');
+            }
+        } else {
+            return redirect('/dashboard');
+        }
     }
 }
