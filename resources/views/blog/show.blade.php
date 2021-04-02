07bfc350bbba3683cb9bc711f932af5b677f068b
Davide <ceredavide@live.it>
2021-04-02 17:18:31 +0200

    aggiunto modal

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -6,11 +6,12 @@
             </div>
             @unless(auth()->user()->id !== $post->user_id)
                 <div class="col-3">
-                    <form id="delete-frm" class="" action="" method="POST">
-                        <a href="/blog/{{ $post->id }}" class="btn btn-warning">Edit Post</a>
-                        @method('DELETE')
+                    <form id="delete-frm" class="" action="/blog/{{ $post->id }}" method="POST">
+                        <a href="/blog/{{ $post->id }}" class="btn btn-warning">Modifica Post</a>
                         @csrf
-                        <button class="btn btn-danger">Delete Post</button>
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">
+                            Elimina Post
+                        </button>
                     </form>
                 </div>
             @endunless
@@ -20,6 +21,28 @@
             <div class="col-12 pt-2">
                 <h1 class="display-one">{{ ucfirst($post->title) }}</h1>
                 <p>{!! $post->body !!}</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="deleteModalLabel">Elimina post</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                   Sei sicuro di voler eliminare il post? L'operazione è irreversibile
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+                    <button type="button" class="btn btn-danger">Elimina</button>
+                </div>
             </div>
         </div>
     </div>
