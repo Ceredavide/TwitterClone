90743d5f97a322a06fddddc47bec26e89bbf5e2a
Davide <ceredavide@live.it>
2021-04-04 20:57:20 +0200

    aggiunto username e data creazione

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,17 +4,19 @@
             <div class="col-12 pt-2">
                 <div class="row">
                     <div class="col-10">
-                        <h1 class="display-one">Our Blog!</h1>
-                        <p>Enjoy reading our posts. Click on a post to read!</p>
+                        <h1 class="display-one text-white">Blog Trap!</h1>
+                        <p class="text-white">Ao spiazzati questo blog</p>
                     </div>
                     <div class="col-2">
-                        <a href="/blog" class="btn btn-success">Add Post</a>
+                        <a href="/blog" class="btn btn-outline-success">Nuovo Post</a>
                     </div>
                 </div>
                 @forelse($posts as $post)
                     <ul class="list-group">
                         <li class="list-group-item my-1">
                             <a href="/dashboard/{{ $post->id }}">{{ ucfirst($post->title) }}</a>
+                            <p>{{ ucfirst($post->user->name) }}</p>
+                            <p>{{ ucfirst($post->created_at->format('d/m/Y')) }}</p>
                         </li>
                     </ul>
                 @empty
